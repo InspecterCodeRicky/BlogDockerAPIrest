9a404cbce3f99fc1aa54dc3a4063970e42b4cd8b
Sentiany Priska Sumampow <vivian.priska@gmail.com>
2021-12-05 19:07:01 +0100

    fix bug

--- app/views/articles.php
+++ app/views/articles.php
@@ -12,57 +12,46 @@
             <div class="line-mf"></div>
           </div>
         </div>
+        <p class='addArticle'><a href="<?=BASEDIR. "add-article"?>">+ ajouter un article</a></p>
       </div>
-      
-      <div class="row">
-
-<<<<<<< HEAD
-<div>
-    <h1>Mes articles </h1>
-    <p><a href="<?=BASEDIR. "add-article"?>">ajouter un article</a></p>
-=======
-        <?php
-            foreach ($data as $article) {?>
-                <div class="col-md-4">
-                    <div class="card card-blog">
-                        <div class="card-img">
-                        <a href="article/<?=$article->getId()?>"><img src="img/post-3.jpg" alt="" class="img-fluid"></a>
-                        </div>
-                        <div class="card-body">
-                        <div class="card-category-box">
-                            <div class="card-category">
-                            <h6 class="category">Web Design</h6>
-                            </div>
-                        </div>
-                        <h3 class="card-title"><a href="article/<?=$article->getId()?>"><?=$article->getTitle()?></a></h3>
-                        <p class="card-description"><?=$article->getContent()?></p>
-                        </div>
-                        <div class="card-footer">
-                        <div class="post-author">
-                            <a href="#">
-                            <img src="img/testimonial-2.jpg" alt="" class="avatar rounded-circle">
-                            <span class="author">Morgan Freeman</span>
-                            </a>
-                        </div>
->>>>>>> c80d88326f1f85623a10c9a8348a30d20d0681d9
-
-                        </div>
-                    </div>
-                </div>
-
-<<<<<<< HEAD
-    <?php
-        foreach ($data as $article) {?>
-            <h4><a href="article/<?=$article->getId()?>"><?=$article->getTitle()?></a></h4>
-            <p><?=$article->getContent()?></p>
-            
-        <?php } ?>
 
     
-</div>
-=======
+      
+    <div class="row">
+
+        
+                <?php
+                    foreach ($data as $article) {?>
+                        <div class="col-md-4">
+                            <div class="card card-blog">
+                                <div class="card-img">
+                                <a href="article/<?=$article->getId()?>"><img src="img/post-3.jpg" alt="" class="img-fluid"></a>
+                                </div>
+                                <div class="card-body">
+                                <div class="card-category-box">
+                                    <div class="card-category">
+                                    <h6 class="category">Web Design</h6>
+                                    </div>
+                                </div>
+                                <h3 class="card-title"><a href="article/<?=$article->getId()?>"><?=$article->getTitle()?></a></h3>
+                                <p class="card-description"><?=$article->getContent()?></p>
+                                </div>
+                                <div class="card-footer">
+                                <div class="post-author">
+                                    <a href="#">
+                                    <img src="img/testimonial-2.jpg" alt="" class="avatar rounded-circle">
+                                    <span class="author">Morgan Freeman</span>
+                                    </a>
+                                </div>
+
+                                </div>
+                            </div>
+                        </div>
+
+
+            
+        
             <?php } ?>      
-      </div>
+    </div>
     </div>
   </section>
->>>>>>> c80d88326f1f85623a10c9a8348a30d20d0681d9
