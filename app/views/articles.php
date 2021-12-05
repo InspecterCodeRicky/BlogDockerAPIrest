3d9001d5de0ea076cd11c8c48011ab97511a7750
Sentiany Priska Sumampow <vivian.priska@gmail.com>
2021-12-05 17:00:02 +0100

    page all articles

--- app/views/articles.php
+++ app/views/articles.php
@@ -1,13 +1,50 @@
+<section id="blog" class="blog-mf sect-pt4 route">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="title-box text-center">
+            <h3 class="title-a">
+                Mes articles
+            </h3>
+            <p class="subtitle-a">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            </p>
+            <div class="line-mf"></div>
+          </div>
+        </div>
+      </div>
+      
+      <div class="row">
 
-<div>
-    <h1>Mes articles </h1>
+        <?php
+            foreach ($data as $article) {?>
+                <div class="col-md-4">
+                    <div class="card card-blog">
+                        <div class="card-img">
+                        <a href="blog-single.html"><img src="img/post-3.jpg" alt="" class="img-fluid"></a>
+                        </div>
+                        <div class="card-body">
+                        <div class="card-category-box">
+                            <div class="card-category">
+                            <h6 class="category">Web Design</h6>
+                            </div>
+                        </div>
+                        <h3 class="card-title"><a href="article/<?=$article->getId()?>"><?=$article->getTitle()?></a></h3>
+                        <p class="card-description"><?=$article->getContent()?></p>
+                        </div>
+                        <div class="card-footer">
+                        <div class="post-author">
+                            <a href="#">
+                            <img src="img/testimonial-2.jpg" alt="" class="avatar rounded-circle">
+                            <span class="author">Morgan Freeman</span>
+                            </a>
+                        </div>
 
+                        </div>
+                    </div>
+                </div>
 
-    <?php
-        foreach ($data as $article) {?>
-            <h4><a href="article/<?=$article->getId()?>"><?=$article->getTitle()?></a></h4>
-            <p><?=$article->getContent()?></p>
-
-        <?php } ?>
-    
-</div>
+            <?php } ?>      
+      </div>
+    </div>
+  </section>
